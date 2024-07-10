3eef54caf21e846646501833165e08098f5cbb68
unknown <csesujon155@gmail.com>
2024-07-10 14:41:39 -0700

    Escape Secquence

---
+++ Basic_Java/BioData.java
@@ -0,0 +1,14 @@
+package Basic_Java;
+
+public class BioData {
+    public static void main(String[] args){
+        System.out.println("Name: MD SUJON AHMED \nMobile: 01680366446, \nB.Sc in CSE");
+        System.out.println("1 \t20 \t30 \t40");
+        System.out.println("1 \t50 \t80 \t90");
+        System.out.println("\"Java Programming\" ");
+        System.out.println("\'Programming is a best programming language\' ");
+        System.out.println("Java Progr \b amming");
+        System.out.println("Java is \rFine");
+        System.out.println("Nickname: \nMD SUJAN AHMED \rSUJAN");
+    }
+}
