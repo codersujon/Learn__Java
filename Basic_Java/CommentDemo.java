ca7f9cf0d2233949aff05ed4b8bc8db3f6d9e388
unknown <csesujon155@gmail.com>
2024-07-10 14:53:48 -0700

    3 types of comments, single line, multiline, documentational comments

---
+++ Basic_Java/CommentDemo.java
@@ -0,0 +1,16 @@
+package Basic_Java;
+/*
+    CommentDemo Class Making
+    Which is belongs to a package.
+    This is known as Multiline Comment
+ */
+public class CommentDemo {
+    public static void main(String[] args){
+        System.out.println("CODERSUJON"); // System -> built in class
+    }
+}
+
+/**
+ * Documentation Comment or Documentational Comment
+ * It's Okay
+ */
